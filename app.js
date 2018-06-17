6e02d42ae9fb39ffd5a8dd9ce6a5becc44e5551e
Liam Stewart <l.stewart@arcane.ws>
2018-06-16 20:56:16 -0400

    Init

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  chrome.tabs.query({
+    'active': true,
+    'lastFocusedWindow': true
+  }, function(tabs) {
+
+    let url = tabs[0].url; //Get current url
+    let parser = document.createElement('a');
+    parser.href = url;
+    let urlFetch = 'http://ip-api.com/json/' + parser.hostname; //url to fetch
+
+    fetch(urlFetch)
+      .then(res => res.json())
+      .then((output) => {
+        document.body.innerHTML = output.query + "😭";
+      })
+      .catch(err => {
+        document.body.innerHTML = '😭';
+      });
+
+  });
+
+}, false);
